ebad594169e8819721f9f7c59816be0003f17726
Build Machine <conda@anaconda.com>
2021-06-30 08:07:49 -0500

    adjust linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/include
-cp include/cudnn.h $PREFIX/include/
+cp include/cudnn*.h $PREFIX/include/
 
 mkdir -p $PREFIX/lib
-cp lib64/libcudnn.so.8.2.1 $PREFIX/lib/
-ln -s $PREFIX/lib/libcudnn.so.8.2.1 $PREFIX/lib/libcudnn.so.8
+cp lib64/libcudnn*.so.* $PREFIX/lib/
+
+#ln -s $PREFIX/lib/libcudnn.so.8.2.1 $PREFIX/lib/libcudnn.so.8
 ln -s $PREFIX/lib/libcudnn.so.8 $PREFIX/lib/libcudnn.so
+
