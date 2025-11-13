c39f275cedfcd39b82d4b71088751ea149d6b4e6
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-11-13 12:03:53 +0100

    add comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ set -e
 
 check-glibc lib/lib*.so*
 
+# Copy all the header files and the shared libraries (including symlinks), but not the static libraries, into the prefix
 mkdir -p $PREFIX/include
 cp -vp include/cudnn*.h $PREFIX/include/
 
